afa759606b2399344e0f0ef2633dd5e836d9504b
Lenna Cooper <lenna.cooper@gmail.com>
2019-01-23 19:02:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+  var dwarfString = [] ; 
+  for (let i = 0; i < dwarves.length; i++ ) {
+   dwarfString += (i+1) + `. ${dwarves[i]} `
+  } 
+  return dwarfString.toString() ;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+var callArray = []  
+ for (let i = 0; i < planeteerCalls.length; i++) {
+   callArray.push(planeteerCalls[i].toUpperCase() + '!')
+ }
+ return callArray
 }
 
 function longPlaneteerCalls(words) {
+  var i = [0]
+  return (words[i].length > 4)
+  }
+  console.log(words.every(longPlaneteerCalls))
+
+
+
+function cheese(foods) {
+return(foods.includes("cheddar" || "gouda" || "camembert"))
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+  if(foods.includes("cheddar" || "gouda" || "camembert")) {
+    return(foods.find(cheese))
+  } else {
+    return("no cheese!")
+  }
 }
